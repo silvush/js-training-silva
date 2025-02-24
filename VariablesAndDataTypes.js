39412b121058be8177f96bbe58627c436ecd0615
silva <silva@DESKTOP-BPP4DD4>
2025-02-24 23:15:07 +0400

    first commit

---
+++ VariablesAndDataTypes.js
@@ -0,0 +1,42 @@
+//Task 1
+const Name = "Silva";
+let counter = 0;
+const Pi = 3.14159;
+let UserAge = 20;
+const colors = ["white", "yellow", "blue"];
+
+//Task 2
+const Surname = "Ghazaryan";
+console.log(typeof surname);
+let Age = 32;
+console.log(typeof Age);
+let isInActive = true;
+console.log(typeof isInActive);
+const UserName = null;
+console.log(typeof UserName);
+let Undefined;
+console.log(typeof undefined);
+let value = Symbol ("My First Homework");
+console.log(typeof value);
+let bigInt = 9007199254740991n;
+console.log(typeof bigInt);
+
+//Task 3 
+const isActive = true;
+console.log(string(isActive));
+const price = "49.99"
+console.log(number(price));
+const hasAccess = false;
+console.log(number(hasAccess));
+const emptyText = "";
+console.log(number(emptyText));
+const username = "JohnDoe";
+console.log(boolean(username));
+const itemCount = 0;
+console.log(boolean(itemCount));
+const notDefined = undefined;
+console.log(number(notDefined));
+const spaceString = "   ";
+console.log(boolean(spaceString));
+const emptyValue = null;
+console.log(string(emptyValue));
